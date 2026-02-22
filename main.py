0b0448d82809245be33392da1d372ab70738969d
isalazera-spec <isalazera@gmail.com>
2026-02-22 20:19:55 +0000

    pass delete after gen

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def gen_password():
+    input_password.delete(0, END)
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -36,6 +37,7 @@ def add_password():
             messagebox.showinfo("Password Added", "Password has been added")
             input_website.delete(0, END)
             input_password.delete(0, END)
+            input_website.focus()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
